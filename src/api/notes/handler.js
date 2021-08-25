979cefe49469486a0d0fff14767561eb58e9a8c9
Irrangga <irranggaa@gmail.com>
2021-08-26 00:21:22 +0700

    postgres database implemented

--- src/api/notes/handler.js
+++ src/api/notes/handler.js
@@ -12,12 +12,12 @@ class NotesHandler {
     this.deleteNoteByIdHandler = this.deleteNoteByIdHandler.bind(this)
   }
 
-  addNoteHandler (request, h) {
+  async addNoteHandler (request, h) {
     try {
       this._validator.validateNotePayload(request.payload)
       const { title = 'untitled', tags, body } = request.payload
 
-      const noteId = this._service.addNote({ title, tags, body })
+      const noteId = await this._service.addNote({ title, tags, body })
 
       const response = h.response({
         status: 'success',
@@ -47,8 +47,8 @@ class NotesHandler {
     }
   }
 
-  getNotesHandler () {
-    const notes = this._service.getNotes()
+  async getNotesHandler () {
+    const notes = await this._service.getNotes()
     return {
       status: 'success',
       data: {
@@ -57,10 +57,10 @@ class NotesHandler {
     }
   }
 
-  getNoteByIdHandler (request, h) {
+  async getNoteByIdHandler (request, h) {
     try {
       const { id } = request.params
-      const note = this._service.getNoteById(id)
+      const note = await this._service.getNoteById(id)
 
       return {
         status: 'success',
@@ -87,12 +87,12 @@ class NotesHandler {
     }
   }
 
-  editNoteByIdHandler (request, h) {
+  async editNoteByIdHandler (request, h) {
     try {
       this._validator.validateNotePayload(request.payload)
       const { id } = request.params
 
-      this._service.editNoteById(id, request.payload)
+      await this._service.editNoteById(id, request.payload)
 
       return {
         status: 'success',
@@ -117,10 +117,10 @@ class NotesHandler {
     }
   }
 
-  deleteNoteByIdHandler (request, h) {
+  async deleteNoteByIdHandler (request, h) {
     try {
       const { id } = request.params
-      this._service.deleteNoteById(id)
+      await this._service.deleteNoteById(id)
       return {
         status: 'success',
         message: 'Note is successfully deleted.'
